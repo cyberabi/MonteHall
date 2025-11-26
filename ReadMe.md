faacb7169f85a3201158a2e63fdf56b8a2381b22
Christopher Burke <chris@cyberabi.com>
2025-11-26 15:03:22 -0800

    Streamlined to 15 lines of code.

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,1 @@
-Monte Carlo simulation of the Monty Hall problem in 25 lines of code (including comments). Uses Python sets to streamline code.
+Monte Carlo simulation of the Monty Hall problem in 15 lines of code (including comments). Uses Python sets to streamline code.
--- main.py
+++ main.py
@@ -1,22 +1,15 @@
-# Monte Carlo (a million trials) meets Monty Hall
 import random
-
 if __name__ == '__main__':
-    trials = 1000000
+    trials = 1000000  # Monte Carlo (a million trials) meets Monty Hall
     for switch in (False, True):
         wins = 0
         for trial in range(trials):
-            # The prize could be anywhere
-            winning_door = random.randint(0, 2)
-            # Pick a door. Any door.
-            first_choice = random.randint(0, 2)
-            # Monty Hall always reveals a goat you didn't pick
-            goats = list({0, 1, 2} - {winning_door, first_choice})
-            goat = goats[random.randint(0, len(goats)-1)]
-            # Will you switch, or not?
+            winning_door = random.randint(0, 2)  # The prize could be anywhere
+            first_choice = random.randint(0, 2)  # Pick a door. Any door.
+            revealable_goats = list({0, 1, 2} - {winning_door, first_choice})
+            goat = random.choice(revealable_goats)  # Monty shows a goat you didn't pick
             other_door = ({0, 1, 2} - {first_choice, goat}).pop()
-            second_choice = first_choice if not switch else other_door
-            if second_choice == winning_door:
-                wins += 1
+            second_choice = first_choice if not switch else other_door  # Will you switch, or not?
+            wins = wins + 1 if second_choice == winning_door else wins
         win_percent = round((float(wins) / trials) * 100, 3)
         print(f'Switch doors: {"Yes" if switch else "No "} Wins: {wins} Losses: {trials-wins} Win Rate: {win_percent}%')
